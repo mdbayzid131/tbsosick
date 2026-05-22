5fcfe501f7d90d7f38113a4e479fc7aeef1058b5
mdbayzid131 <bayazidbayazid567@gmail.com>
2026-05-23 02:55:36 +0600

    feat: add update package bottom sheet with tiered subscription plans selection

--- lib/presentation/screens/ProfilePage/update_payment_method_bottom.dart
+++ lib/presentation/screens/ProfilePage/update_payment_method_bottom.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
-
-
-
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tbsosick/config/themes/app_theme.dart';
 import 'package:tbsosick/presentation/widgets/custom_elevated_button.dart';
