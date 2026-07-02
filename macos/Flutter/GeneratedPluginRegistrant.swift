bf6d2068ba984e8c353844c85f820a9f3baafe98
mdbayzid131 <bayazidbayazid567@gmail.com>
2026-07-03 04:25:57 +0600

    chore: update project dependencies and initialize native plugin configurations

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,6 @@ import google_sign_in_ios
 import in_app_purchase_storekit
 import open_file_mac
 import package_info_plus
-import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sign_in_with_apple
@@ -31,7 +30,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
